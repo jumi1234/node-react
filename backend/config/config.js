35c632c2bad09b0b12e8ffbf4392b09cec79247a
jumi1234 <farewellsad89@gmail.com>
2021-04-29 16:55:52 +0900

    backend에서 rest api 설정, mySQL 연동

---
+++ backend/config/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  HOST: "localhost",
+  USER: "root",
+  PASSWORD: "12341234",
+  DB: "shabping"
+};
---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// parse requests of content-type: application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type: application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ test: "success!" });
+});
+
+require("./routes/customer.js")(app);
+
+// set port, listen for requests
+app.listen(5000, () => {
+  console.log("Server is running on port 5000.");
+});
